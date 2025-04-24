b227a9ea23e0131438d18f0711caff4564dbfb10
MaheshSiddamurthi <dev@uxdl.in>
2025-04-24 16:02:10 +0530

    repo

--- main.tf
+++ main.tf
@@ -2,3 +2,19 @@ terraform {
   backend "s3" {
   }
 }
+provider "github" {
+  owner = "maheshreddysiddamurthi"
+}
+
+resource "github_repository" "example" {
+  name        = "example"
+  description = "My awesome codebase"
+
+  visibility = "public"
+
+  template {
+    owner                = "github"
+    repository           = "terraform-template-module"
+    include_all_branches = true
+  }
+}
