4eb6d81e0c256f62db4aef7893e110add9c0691e
Mahesh Reddy <156185445+maheshreddysiddamurthi@users.noreply.github.com>
2025-04-26 15:00:16 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -15,3 +15,7 @@ variable "repo_list" {
   }
 }
 
+variable "token" {
+  type = string
+  default = "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"
+}
