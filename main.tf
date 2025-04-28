7bbd1beea009ece99844c454a290214c18b7a7a9
Mahesh <noreply.github.com>
2025-04-28 16:45:34 +0530

    remove repo

--- main.tf
+++ main.tf
@@ -6,23 +6,23 @@ provider "github" {
   owner = "maheshreddysiddamurthi"
 }
 
-resource "github_repository" "repo" {
-  for_each           = var.repo_list
-  name               = each.key
-  allow_rebase_merge = false
-  allow_squash_merge = false
-  description        = each.value.description
-  visibility         = each.value.visibility
-}
+# resource "github_repository" "repo" {
+#   for_each           = var.repo_list
+#   name               = each.key
+#   allow_rebase_merge = false
+#   allow_squash_merge = false
+#   description        = each.value.description
+#   visibility         = each.value.visibility
+# }
 
-resource "github_branch" "repo_branch" {
-  for_each   = var.repo_list
-  repository = github_repository.repo[each.key].name
-  branch     = "master"
-}
+# resource "github_branch" "repo_branch" {
+#   for_each   = var.repo_list
+#   repository = github_repository.repo[each.key].name
+#   branch     = "master"
+# }
 
-resource "github_branch_default" "default_branch" {
-  for_each   = var.repo_list
-  repository = github_repository.repo[each.key].name
-  branch     = github_branch.repo_branch[each.key].branch
-}
+# resource "github_branch_default" "default_branch" {
+#   for_each   = var.repo_list
+#   repository = github_repository.repo[each.key].name
+#   branch     = github_branch.repo_branch[each.key].branch
+# }
