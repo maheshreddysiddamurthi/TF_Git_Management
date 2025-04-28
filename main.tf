7129cbcb36b6226e4d4a2b8d81f8067645c292a6
Mahesh <noreply.github.com>
2025-04-28 16:49:14 +0530

    username update

--- main.tf
+++ main.tf
@@ -6,23 +6,29 @@ provider "github" {
   owner = "maheshreddysiddamurthi"
 }
 
-# resource "github_repository" "repo" {
-#   for_each           = var.repo_list
-#   name               = each.key
-#   allow_rebase_merge = false
-#   allow_squash_merge = false
-#   description        = each.value.description
-#   visibility         = each.value.visibility
-# }
+resource "github_repository" "repo" {
+  for_each           = var.repo_list
+  name               = each.key
+  allow_rebase_merge = false
+  allow_squash_merge = false
+  description        = each.value.description
+  visibility         = each.value.visibility
+  auto_init          = true
+}
 
-# resource "github_branch" "repo_branch" {
-#   for_each   = var.repo_list
-#   repository = github_repository.repo[each.key].name
-#   branch     = "master"
-# }
+resource "time_sleep" "wait_30_seconds" {
+  depends_on      = [github_repository.repo]
+  create_duration = "30s"
+}
 
-# resource "github_branch_default" "default_branch" {
-#   for_each   = var.repo_list
-#   repository = github_repository.repo[each.key].name
-#   branch     = github_branch.repo_branch[each.key].branch
-# }
+resource "github_branch" "repo_branch" {
+  for_each   = var.repo_list
+  repository = github_repository.repo[each.key].name
+  branch     = "master"
+}
+
+resource "github_branch_default" "default_branch" {
+  for_each   = var.repo_list
+  repository = github_repository.repo[each.key].name
+  branch     = github_branch.repo_branch[each.key].branch
+}
